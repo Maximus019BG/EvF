ab9d48d108c23a21be9e267435dcec87778d64ba
Maximus019BG <maxralev@abv.bg>
2024-05-18 14:23:52 +0300

    Indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] },)
 
@@ -16,7 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='dark:bg-[#011E2B] bg-white'>
-      <meta name="google" content="The best events in Varna BG" key="notranslate" />
+        <meta name="google" content="The best events in Varna BG" key="notranslate" />
+        <meta name="google-site-verification" content="kptgP0ZcFIb-H7uFL3eic-gblPSt0A2HhpvhcyHHadU" />
       <body className={inter.className}>{children}</body>
     </html>
   )
