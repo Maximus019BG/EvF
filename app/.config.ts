c351c402dfe7aae0c248077f8ec7a1ac57829986
Maximus019 <123507237+Maximus019BG@users.noreply.github.com>
2024-06-02 08:31:25 +0300

    Update .config.ts

--- app/.config.ts
+++ app/.config.ts
@@ -3,7 +3,7 @@ interface Config {
 }
 
 const config: Config = {
-  apiUrl: 'https://b27381cr-5000.euw.devtunnels.ms',
+  apiUrl: 'https://zx6ghtk2-5000.euw.devtunnels.ms',
 };
 
 export default config;  
